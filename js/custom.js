17277caff473248e36d463c539204aae349c4581
Selvakumar <selvarmd@gmail.com>
2018-06-27 15:07:10 +0530

    Social Icon added

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    /****** Design 1 ********/
+    $('.design1 .toggle').click(function () {
+        $('.design1 .toggle').toggleClass('active');
+        $('.design1 ul').toggleClass('active');
+    })
+
+})
